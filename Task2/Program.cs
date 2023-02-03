fe869e61f12d4a22e223779e8f3a512c247c3573
AleksKolychev <aleksushome@gmail.com>
2023-02-03 14:21:11 +0500

    Add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Введите любое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n > 1000)
+{
+   n = n / 10;
+}
+if (n < 1000 && n > 99)
+    Console.WriteLine(n % 10);
+else if (n < 100)
+    Console.WriteLine("Третей цифры нет");
