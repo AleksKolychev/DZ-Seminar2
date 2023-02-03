3ff99eb1acacf00b6c4bcd85355caa5a48aa277a
AleksKolychev <aleksushome@gmail.com>
2023-02-03 15:48:16 +0500

    Add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("Введите цифру, обозначающую день недели: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n == 1)
+    Console.WriteLine("no");
+else if (n == 2)
+    Console.WriteLine("no");
+else if (n == 3)
+    Console.WriteLine("no");
+else if (n == 4)
+    Console.WriteLine("no");
+else if (n == 5)
+    Console.WriteLine("no");
+else if (n == 6)
+    Console.WriteLine("yes");
+else if (n == 7)
+    Console.WriteLine("yes");
